a05d20d95812644c8a2e67a25dce14b629635d33
cmtp <christian.tola@hotmail.com>
2016-07-21 17:27:22 -0400

    add mongoose module

--- src/models/users.js
+++ src/models/users.js
@@ -1,75 +1,10 @@
-var q = require('q');
-var id = 1;
-var users = [];
-/**
- * USER
- * name: string,
- * email:string,
- * birthDate: string
- */
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
 
-function create(user) {
-    user.id = id;
-    users.push(user);
-    id++;
-    return q.resolve(user);
-}
+var UserSchema = new Schema({
+    name: { type: String, required: true },    
+    email: { type: String, required: true },
+    birthday:  { type: Date, required: true }
+});
 
-function find(params) {
-    if(!params) {
-        return q.resolve(users);
-    }
-    var result = users.filter(function (user) {
-        var found = false;
-        for( var param in params) {
-            if(user[param] && user[param] == params[param]) {
-                found = true;
-            }
-        }
-        return found;
-    });
-    return q.resolve(result);
-}
-
-function findById(params) {
-    var result = users.find(function (user) {
-        var found = false;
-        for (var param in params) {
-            if (user[param] && user[param] == params[param]) {
-                found = true;
-            }
-        }
-        return found;
-    });
-
-    return q.resolve(result);
-}
-
-function updateById(userId, user) {
-    var index = users.findIndex(function (u) {
-        return u.id == userId;
-    });
-    users[index] = user;
-    users[index].id = userId;
-
-    return q.resolve(users[index]);
-}
-
-function deleteId(userId) {
-    var index = users.findIndex(function (u) {
-        return u.id == userId;
-    });
-
-    var userCopy = users[index];
-    users.splice(index,1);
-
-    return q.resolve(userCopy);
-}
-
-module.exports  = {
-    create: create,
-    find: find,
-    findById: findById,
-    updateById: updateById,
-    deleteId: deleteId
-}
+module.exports = mongoose.model('User', UserSchema)
--- src/server.js
+++ src/server.js
@@ -1,132 +1,174 @@
-// Dependencies
-// var q = require('q');
-var express = require('express');
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-//init app express
+var express = require('express')
+var morgan = require('morgan')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
 var app = express();
 
-var Users = require('./models/users.js');
-var Posts = require('./models/posts.js')
+//------- database initialization
+require('mongoose').Promise = require('q').Promise;
+mongoose.connect('mongodb://localhost:27017/mean-auth');
 
-//add plugins to express
-app.use(bodyParser.urlencoded({extended: true}));
+//------- setting express middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-// POST registrar usuario
-app.post('/users', function (req, res) {
-    var user = req.body;
-    // var result = Users.create(user);
-    Users.create(user).then(function(result) {
-        res.send(result);
-    }).catch(function(err) {
-        console.log(err);
-    });
-});
-// GET lista de usuarios
-app.get('/users', function(req, res) {
-    // .find({name: 'chris'})
-    Users.find().then(function(result) {
-        res.send(result);
-    }).catch(function(err) {
-        console.log(err);
-    });
-});
-//GET mostrar un usuario por su id
-app.get('/users/:userId', function(req, res) {
-    var userId = req.params.userId;
-    Users.findById({id: userId}).then(function(result) {
-        res.send(result);
-    }).catch(function(err) {
-        console.log(err);
-    });
-});
-// PUT actualizar usuario
-app.put('/users/:userId', function(req,res) {
-    var userId = req.params.userId;
-    Users.updateId(userId, req.body).then(function (result) {
-        res.send(result);
-    }).catch(function(err) {
-        console.log(err);
-    });
-});
-// DELETE remover un usuario
-app.delete('/users/:userId', function(req,res) {
-    var userId = req.params.userId;
+//------- importing models
+var Users = require('./models/users.js')
+var Posts = require('./models/posts.js')
 
-    Users.deleteId(userId).then(function(result) {
-        res.send(result);
-    }).catch(function (err) {
-        console.log(err);
-    });
+//------ controllers
+app.get('/users', function (req, res) {
+  Users.find().then(function (result) {
+    res.send(result)
+  }).catch(function (err) {
+    console.log(err)
+  })
 });
+
+app.get('/users/:userId', function (req, res) {
+  var userId = req.params.userId;
+  Users.findOne({_id: userId}).then(function (result) {
+    res.send(result)
+  }).catch(function (err) {
+    console.log(err)
+  })
+});
+
+app.post('/users', function (req, res) {
+  Users.create(req.body).then(function (result) {
+    res.send(result)
+  }).catch(function (err) {
+    console.log(err)
+  })
+})
+
+app.put('/users/:userId', function (req, res) {
+  var userId = req.params.userId;
+
+  Users.findByIdAndUpdate(userId, req.body, {new: true}).then(function (result) {
+    res.send(result)
+  }).catch(function (err) {
+    console.log(err)
+  })
+})
+
+app.delete('/users/:userId', function (req, res) {
+  var userId = req.params.userId;
+
+  Users.findByIdAndRemove(userId).then(function (result) {
+    res.send(result)
+  }).catch(function (err) {
+    console.log(err)
+  })
+})
+
 
 //--------------------------------------------------------------
 app.get('/users/:userId/posts', function (req, res) {
-  var userId = req.params.userId;
-  Posts.find({userId: userId}).then(function (result) {
-    res.send(result)
-  }).catch(function (err) {
-    console.log(err)
-  })
+  var threeDaysAgo = new Date()
+  threeDaysAgo.setDate(threeDaysAgo.getDate() - 3)
+
+  var userId = req.params.userId
+  var from = req.query.from || threeDaysAgo
+  var to = req.query.to || new Date()
+
+  Posts.find({userId: userId, createdAt: {"$gte": from, "$lt": to}})
+    .then(function (result) {
+      res.send(result)
+    }).catch(function (err) {
+      console.log(err)
+    })
 });
 
 app.get('/users/:userId/posts/:postId', function (req, res) {
-  var postId = req.params.postId;
-  Posts.findOne({id: postId}).then(function (result) {
-    res.send(result)
-  }).catch(function (err) {
-    console.log(err)
-  })
+  var postId = req.params.postId
+  Posts.findOne({_id: postId})
+    .then(function (result) {
+      res.send(result)
+    }).catch(function (err) {
+      console.log(err)
+    })
 });
 
 app.post('/users/:userId/posts', function (req, res) {
-  Posts.create(req.body).then(function (result) {
-    res.send(result)
-  }).catch(function (err) {
-    console.log(err)
-  })
+  Posts.create(req.body)
+    .then(function (result) {
+      res.send(result)
+    }).catch(function (err) {
+      console.log(err)
+    })
 })
 
 app.put('/users/:userId/posts/:postId', function (req, res) {
-  var postId = req.params.postId;
+  var postId = req.params.postId
 
-  Posts.findByIdAndUpdate(postId, req.body).then(function (result) {
-    res.send(result)
-  }).catch(function (err) {
-    console.log(err)
-  })
+  Posts.findByIdAndUpdate(postId, req.body, {new: true})
+    .then(function (result) {
+      res.send(result)
+    }).catch(function (err) {
+      console.log(err)
+    })
 })
 
 app.delete('/users/:userId/posts/:postId', function (req, res) {
-  var postId = req.params.postId;
+  var postId = req.params.postId
 
-  Posts.findByIdAndRemove(postId).then(function (result) {
-    res.send(result)
-  }).catch(function (err) {
-    console.log(err)
-  })
+  Posts.findByIdAndRemove(postId)
+    .then(function (result) {
+      res.send(result)
+    }).catch(function (err) {
+      console.log(err)
+    })
 })
 
-// listener
+//--------------------------------------------------------------
+app.get('/posts/:postId/comments', function (req, res) {
+  var postId = req.params.postId
+
+  Posts.findOne({_id: postId})
+    .then(function (result) {
+        res.send(result.comments)
+    }).catch(function (err) {
+      console.log(err)
+    })
+})
+
+app.post('/posts/:postId/comments', function (req, res) {
+  var postId = req.params.postId
+
+  Posts.findByIdAndUpdate(postId, {$push: {"comments": req.body } }, { new: true })
+    .then(function (result) {
+        res.send(result)
+    }).catch(function (err) {
+      console.log(err)
+    })
+})
+
+app.put('/posts/:postId/comments/:commentId', function (req, res) {
+  var postId = req.params.postId
+  var commentId = req.params.commentId
+
+  Posts.update({'comments._id': commentId }, { $set: { "comments.$.text": req.body.text } }, { new: true })
+    .then(function (result) {
+        res.send(result)
+    }).catch(function (err) {
+      console.log(err)
+    })
+})
+
+app.delete('/posts/:postId/comments/:commentId', function (req, res) {
+  var postId = req.params.postId
+  var commentId = req.params.commentId
+
+  Posts.findByIdAndUpdate(postId, {$pull: {"comments": { _id: commentId } } }, { new: true })
+    .then(function (result) {
+        res.send(result)
+    }).catch(function (err) {
+      console.log(err)
+    })
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
-
-/**
- * 
- * GET /usuarios => lista de usuarios
- * GET /usuarios/:usuarioId => usuario especifico
- * POST /usuarios => registrar un usuario
- * PUT /usuarios:usuarioId => modificar un usuario
- * DELETE /usuarios/:usuarioId => eliminar un usuario
- * /publicaciones
- * /comentarios
- * 
- * 
- * posts
- * 
- * 
- */
